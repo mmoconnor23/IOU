aa130e8a79fdd548a511e5c86d8ae15fe4b5db4c
Mallory <m.pitser@gmail.com>
2014-01-25 11:11:04 -0500

    added add debtor button to action bar

--- IOU/gen/com/example/iou/R.java
+++ IOU/gen/com/example/iou/R.java
@@ -21,27 +21,33 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_new=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_add_debtor=0x7f080005;
+        public static final int action_settings=0x7f080006;
+        public static final int debt_description=0x7f080003;
         public static final int debt_name=0x7f080001;
+        public static final int debtor_name=0x7f080002;
         public static final int pager=0x7f080000;
-        public static final int section_label=0x7f080002;
+        public static final int section_label=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int add_debt=0x7f030001;
-        public static final int fragment_main_dummy=0x7f030002;
+        public static final int debt=0x7f030002;
+        public static final int fragment_main_dummy=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int action_add_debtor=0x7f050001;
+        public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int title_section1=0x7f050002;
-        public static final int title_section2=0x7f050003;
+        public static final int title_section1=0x7f050003;
+        public static final int title_section2=0x7f050004;
     }
     public static final class style {
         /** 
