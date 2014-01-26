1db728ed256a383a2e38ec3bfdd25fc5248eaebe
Mallory <m.pitser@gmail.com>
2014-01-25 20:48:12 -0500

    added spinner to the add debt age

--- IOU/src/com/example/iou/MainActivity.java
+++ IOU/src/com/example/iou/MainActivity.java
@@ -166,9 +166,9 @@ public class MainActivity extends FragmentActivity implements
 			Locale l = Locale.getDefault();
 			switch (position) {
 			case 0:
-				return getString(R.string.title_section1).toUpperCase(l);
+				return getString(R.string.iou).toUpperCase(l);
 			case 1:
-				return getString(R.string.title_section2).toUpperCase(l);
+				return getString(R.string.uome).toUpperCase(l);
 			}
 			return null;
 		}
